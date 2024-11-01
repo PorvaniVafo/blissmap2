043e6659fb4aa773f87d028cdc9104ef43ae424c
PorvaniVafo <parvanivafa.mokhammad@alatoo.edu.kg>
2024-11-01 17:04:36 +0600

    Initial commit

--- main/java/entity/Doctor.java
+++ main/java/entity/Doctor.java
@@ -11,33 +11,26 @@ import lombok.experimental.FieldDefaults;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Doctor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-
-    @Column
-    String name;
-
-    @Column
-    Double rating;
-
-    @Column
-    String phoneNumber;
-
-    @Column
-    String email;
+    private Long id;
+    private String name;
+    private double rating;
+    private String phoneNumber;
+    private String email;
 
 //    @ManyToOne
 //    @JoinColumn(name = "specialty_id", nullable = false)
-//    Specialty specialty;
+//    private Specialty specialty;
 //
 //    @ManyToOne
 //    @JoinColumn(name = "clinic_id", nullable = false)
-//    Clinic clinic;
+//    private Clinic clinic;
 //
 //    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL)
-//    List<Review> reviews;
+//    private List<Review> reviews;
+
+
 }
